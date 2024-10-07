b2a2732d87d34b3b07c43a4259392563d3a02b78
Ralf Van Nieuwenhuyse <ralfvn2003@gmail.com>
2024-10-07 15:48:48 +0200

    added loading bar when loading model

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
+
+  if (event.detail.totalProgress === 0) {
+    progressBar.style.display = 'block';
+    updatingBar.style.width = '0%';
   } else {
-    progressBar.classList.remove('hide');
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+
+    if (event.detail.totalProgress === 1) {
+      setTimeout(() => {
+        progressBar.style.display = 'none';
+      }, 500);
+    }
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+//https://github.com/google/model-viewer/discussions/4142
